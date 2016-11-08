e632d39a15f577306e78c22aa461c646c1190d54
Dmitry Shirokov <deadrunk@gmail.com>
2016-11-08 17:09:18 +1100

    Working version

--- test/index.js
+++ test/index.js
@@ -6,6 +6,8 @@ describe('ahm-config', () => {
   process.env.NODE_CONFIG_DIR = `${__dirname}/data`;
   process.env.NODE_CONFIG_FILE = `${__dirname}/data/override.config.json`;
   process.env.e = 55555;
+  process.env.g = 666666;
+  process.env.f__baz = 'Should not be shared';
 
   it('should return proper config values from hierarchy', () => {
     const store = config.make();
@@ -14,6 +16,7 @@ describe('ahm-config', () => {
     assert.equal(store.get('c'), 333, 'Value should be taken from custom config');
     assert.equal(store.get('d'), 444, 'Value should NOT be taken from .env file');
     assert.equal(store.get('e'), 55555, 'Value should be taken from env variables');
+    assert.equal(store.get('g'), undefined, 'Value should not be taken from env vars');
   });
 
   it('should support isolated stores', () => {
@@ -29,5 +32,13 @@ describe('ahm-config', () => {
     const store = config.make();
     assert.equal(store.get('NODE_ENV'), 'staging'); // overriden above
     assert.equal(store.get('PORT'), 3000);
+  });
+
+  it('should suport schema matching and environment variables isolation', () => {
+    const store = config.make();
+    assert.equal(store.get('a'), 1);
+    assert.equal(store.get('e'), 55555);
+    assert.equal(store.get('f:foo'), 'bar');
+    assert.equal(store.get('f:baz'), undefined);
   });
 });
