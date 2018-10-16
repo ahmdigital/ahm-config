cafd4384bce849b84fd56d44a445ecedf7d72b65
Dmitry Shirokov <deadrunk@gmail.com>
2018-10-16 11:56:01 +1100

    [minor] Add example

---
+++ example/app.js
@@ -0,0 +1,10 @@
+const config = require('@ahmdigital/config').make({
+  path: `${__dirname}/config`,
+  normalize: true,
+});
+
+console.log('Whole config:');
+console.log(config.get());
+
+console.log('Server details:');
+console.log(`http://${config.get('server:host')}:${config.get('server:port')}`);
